2ad945693bdbbd150569b882ac37cbbd9adab941
Dansware03 <dansware2003@gmail.com>
2024-12-08 17:45:21 -0400

    first commit

---
+++ includes/class-system_grzn_api-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://https://github.com/Dansware03
+ * @since      1.0.0
+ *
+ * @package    System_grzn_api
+ * @subpackage System_grzn_api/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    System_grzn_api
+ * @subpackage System_grzn_api/includes
+ * @author     Dansware Dev <dansware2003@gmail.com>
+ */
+class System_grzn_api_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-system_grzn_api-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://https://github.com/Dansware03
+ * @since      1.0.0
+ *
+ * @package    System_grzn_api
+ * @subpackage System_grzn_api/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    System_grzn_api
+ * @subpackage System_grzn_api/includes
+ * @author     Dansware Dev <dansware2003@gmail.com>
+ */
+class System_grzn_api_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
